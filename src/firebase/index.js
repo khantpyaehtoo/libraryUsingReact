754992116695ff7182e3704cd09c95194aee9478
khantpyaehtoo <ericrebillet@gmail.com>
2026-03-07 20:50:43 +0630

    library

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCwxTnUZVAyvGdxBgcq_MSfKUkN45D3jW8",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 let db = getFirestore(app);
+let auth = getAuth(app);
 
-export { db };
+export { db, auth };
---
+++ src/hooks/useSignup.js
@@ -0,0 +1,27 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { auth } from "../firebase";
+
+export default function useSignup() {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const signUp = async (email, password) => {
+        try {
+            setLoading(true);
+            let res = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password,
+            );
+            setError("");
+            setLoading(false);
+            return res.user;
+        } catch (e) {
+            setLoading(false);
+            setError(e.message);
+        }
+    };
+
+    return { error, loading, signUp };
+}
