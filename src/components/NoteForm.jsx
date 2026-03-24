65747b6fb6abcf72e231d1e8dc1db251bac13132
khantpyaehtoo <ericrebillet@gmail.com>
2026-03-24 15:50:10 +0630

    update

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import useFirestore from "../hooks/useFirestore";
 
-export default function NoteForm() {
+export default function NoteForm({ type = "create", setEditNote, editNote }) {
     let { id } = useParams();
     let [body, setBody] = useState("");
 
     let { addCollection } = useFirestore();
+
+    useEffect(() => {
+        if (type === "update") {
+            setBody(editNote.body);
+        }
+    }, [type]);
 
     let addNote = async (e) => {
         e.preventDefault();
@@ -27,12 +33,23 @@ export default function NoteForm() {
                 cols="30"
                 rows="5"
             ></textarea>
-            <button
-                type="submit"
-                className="text-white bg-primary px-3 py-2 rounded-lg my-3 flex items-center gap-2 cursor-pointer"
-            >
-                <span>Add Note</span>
-            </button>
+            <div className="flex space-x-3">
+                <button
+                    type="submit"
+                    className="text-white bg-primary px-3 py-2 rounded-lg my-3 flex items-center gap-2 cursor-pointer"
+                >
+                    <span>{type === "create" ? "Add" : "Update"} Note</span>
+                </button>
+                {type === "update" && (
+                    <button
+                        type="button"
+                        onClick={() => setEditNote(null)}
+                        className="text-primary border-2 border-primary px-3 py-2 rounded-lg my-3 flex items-center gap-2 cursor-pointer"
+                    >
+                        cancel
+                    </button>
+                )}
+            </div>
         </form>
     );
 }
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,10 +1,17 @@
 import trashIcon from "../assets/trash.svg";
-import useFirestore from "../hooks/useFirestore";
-import { useParams } from "react-router-dom";
+import editIcon from "../assets/edit.svg";
 import moment from "moment";
+
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+
+import useFirestore from "../hooks/useFirestore";
+import NoteForm from "../components/NoteForm";
 
 export default function NoteList() {
     let { id } = useParams();
+    let [editNote, setEditNote] = useState(null);
+
     let { getCollection, deleteDocument } = useFirestore();
     let {
         error,
@@ -29,15 +36,31 @@ export default function NoteList() {
                                 {moment(note?.date?.seconds * 1000).fromNow()}
                             </div>
                         </div>
-                        <div onClick={() => deleteNote(note.id)}>
+                        <div>
                             <img
+                                onClick={() => deleteNote(note.id)}
                                 className="cursor-pointer"
                                 src={trashIcon}
                                 alt=""
                             />
+                            <img
+                                onClick={() => setEditNote(note)}
+                                className="cursor-pointer"
+                                src={editIcon}
+                                alt=""
+                            />
                         </div>
                     </div>
-                    <div className="mt-3">{note.body}</div>
+                    <div className="mt-3">
+                        {editNote?.id !== note.id && note.body}
+                        {editNote?.id === note.id && (
+                            <NoteForm
+                                type="update"
+                                setEditNote={setEditNote}
+                                editNote={editNote}
+                            />
+                        )}
+                    </div>
                 </div>
             </div>
         ))
